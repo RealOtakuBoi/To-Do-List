6c53a685123f7ec9ca736584cf9a037ae4eb9ae2
realotakuboi <sohamseth2005@gmail.com>
2022-07-22 10:52:34 +0530

    Scripting Start

---
+++ index.js
@@ -0,0 +1,24 @@
+const listContainer = document.querySelector('[data-lists]');
+
+let lists = ['name','todo'];
+
+function render(){
+    clearElement(listContainer);
+
+    lists.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.classList.add('list-name');
+        listElement.innerText = list;
+        listContainer.appendChild(listElement)
+    })
+}
+
+
+function clearElement(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild)
+    }
+}
+
+render();
+
