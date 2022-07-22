c6dbf817961a543c15f8fae81f0ceae2e4092f85
realotakuboi <sohamseth2005@gmail.com>
2022-07-22 11:26:59 +0530

    Local Storage Implemented

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const listContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
 
-let lists = [];
+
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
 
-newListForm.addEventListener('submit', (e) => {
+newListForm.addEventListener('submit', e => {
     e.preventDefault();
     const listName = newListInput.value;
     if(listName == null || listName === '') return;
     const list = createList(listName);
     newListInput.value = null;
     lists.push(list);
-    render();
+    saveAndRender();
 });
 
 function createList(name){
     return {id: Date.now().toString(), name:name,tasks: [] }
+}
+
+function saveAndRender(){
+    save();
+    render();
+}
+
+function save(){
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
 }
 
 function render(){
@@ -27,6 +40,9 @@ function render(){
         listElement.dataset.listId = list.id;
         listElement.classList.add('list-name');
         listElement.innerText = list.name;
+        if(list.id === selectedListId) {
+            listElement.classList.add('active-list');
+        }
         listContainer.appendChild(listElement)
     })
 }
