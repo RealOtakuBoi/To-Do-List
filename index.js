e8c0156ed74922ae2e5a3c5655b7b67deb2a640f
realotakuboi <sohamseth2005@gmail.com>
2022-07-22 11:09:23 +0530

    Lists can now be added

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const listContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
 
-let lists = ['name','todo'];
+let lists = [];
+
+
+newListForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const listName = newListInput.value;
+    if(listName == null || listName === '') return;
+    const list = createList(listName);
+    newListInput.value = null;
+    lists.push(list);
+    render();
+});
+
+function createList(name){
+    return {id: Date.now().toString(), name:name,tasks: [] }
+}
 
 function render(){
     clearElement(listContainer);
 
     lists.forEach(list => {
         const listElement = document.createElement('li');
+        listElement.dataset.listId = list.id;
         listElement.classList.add('list-name');
-        listElement.innerText = list;
+        listElement.innerText = list.name;
         listContainer.appendChild(listElement)
     })
 }
